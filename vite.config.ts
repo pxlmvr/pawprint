53add809f038e0f5a8f3f364d791bb86d5544611
pxlmvr <matteo.losi@casavo.com>
2023-10-31 16:33:37 +0100

    feat: configure path aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@containers': path.resolve(__dirname, './src/containers'),
+    },
+  },
   plugins: [react()],
 })
